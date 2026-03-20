4f112f4b6f2731a792346069ce8174b647b659b0
nasan92 <nathanael.santschi@outlook.com>
2026-03-20 12:10:34 +0100

    clean up storage options

--- js/Config.js
+++ js/Config.js
@@ -1,8 +1,5 @@
 mindmaps.Config = function() {
     return {
-        MindMapAddress: "https://yourwebsite.com/s3/process.php",
-        MindMapListAddress: "https://yourwebsite.com/s3/processFilenames.php",
-        BaseUrl: "https://yourwebsite.com/",
         activateDirectUrlInput: true,
         activateUrlsFromServerWithoutSearch: false,
         activateUrlsFromServerWithSearch: false,
