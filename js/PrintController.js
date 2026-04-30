1bd66d0eda10c86a68dd7ab865c9bb74e85af1f1
nasan92 <nathanael.santschi@outlook.com>
2026-04-30 07:07:07 +0200

    use svg renderer for print option

--- js/PrintController.js
+++ js/PrintController.js
@@ -1,12 +1,15 @@
 mindmaps.PrintController = function(e, t, n) {
     function s() {
-        var e = i.renderAsPNG(n.getDocument());
-        $("#print-area").html(e);
-        window.print()
+        var svgRenderer = new mindmaps.StaticSVGRenderer;
+        var svgString = svgRenderer.render(n.getDocument());
+        var svgContent = svgString.replace(/^<\?xml[^]*?\?>\s*/, '');
+        $("#print-area").html(svgContent);
+        setTimeout(function() {
+            window.print();
+        }, 250);
     }
     var r = t.get(mindmaps.PrintCommand);
     r.setHandler(s);
-    var i = new mindmaps.StaticCanvasRenderer;
     e.subscribe(mindmaps.Event.DOCUMENT_CLOSED, function() {
         r.setEnabled(false)
     });
